f521207547ccf5492f4a0821909af7dd0be7a04b
Aditi Nair <asn264@nyu.edu>
2015-12-05 21:40:43 -0500

    More edits

--- ReportCardGenerator/FileName.py
+++ ReportCardGenerator/FileName.py
@@ -8,8 +8,9 @@ import os.path
 
 def prompt_for_filename():
 
-	'''Asks the user to choose a filename. Concatenates ".pdf" and returns the string.'''
-	return raw_input("Please enter a filename for your report. Entering \"report\" will generate the file \"report.pdf\".")+ ".pdf"
+	'''Asks the user to choose a filename.'''
+	return raw_input("Please enter a filename for your report using only alphanumeric characters and/or underscores. Entering \"report\" will generate the file \"report.pdf\".")
+
 
 def check_filename_exists(user_input):
 
@@ -17,15 +18,52 @@ def check_filename_exists(user_input):
 
 	return os.path.exists(user_input)
 
-def get_filename(user_input):
 
-	'''Takes a filename - usually user input. If the file exists, warns the user and forces them to choose another filename.'''
+def check_legal_filename(user_input):
 
-	if check_filename_exists(user_input):
-		print "This file already exists in the current directory. Please enter another filename."
-		return get_filename()
-		
+	'''Ensures that filenames are safe. Conservatively, we will force the user to only use filenames that have alphanumeric characters and underscores.'''
+	
+	#If the string is fully alphanumeric, return True - it *is* legal.
+	if user_input.isalnum():
+		return True
+
+	#If there are characters besides alphanumeric characters, we will only allow underscores.
 	else:
+
+		#Check if the first element is an underscore. If so ignore it below.
+		if user_input[0] == "_":
+			user_input = user_input[1:]
+
+		#Check if the last element is an underscore. If so ignore it below.
+		if user_input[-1] == "_":
+			user_input = user_input[0:-1]
+
+		#Try splitting by "_" and seeing if the substrings are alphanumeric. Return True if all of the substrings are alphanumeric.
+		return all([substring.isalnum() for substring in user_input.split("_")])
+
+
+def get_filename():
+
+	'''Takes user input for a filename. If the file exists, warns the user and forces them to choose another filename. Only allows legal filenames.
+	Returns passing filenames with .pdf concatenated.'''
+
+	user_input = prompt_for_filename()
+
+	if user_input == "quit":
+		sys.exit()
+
+	else:
+		#At this point, we have not concatenated ".pdf" to user_input. We check that the prefix is legal.
+		if not check_legal_filename(user_input):
+			print "Illegal filename: enter a string containing only alphanumeric characters and/or underscores."
+			return get_filename()
+
+		#In order to check whether the file exists, we need to concatenate .pdf
+		user_input = user_input + ".pdf"
+		if check_filename_exists(user_input): 
+			print "This file already exists in the current directory. Please enter another filename."
+			return get_filename()
+
 		return user_input
 
-print check_filename_exists(prompt_for_filename())
+
